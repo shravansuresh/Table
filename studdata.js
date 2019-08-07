dcb16df2e5dd7a73b75973086cea2b7ce42f6aec
shravan <shravan@qburst.com>
2019-08-07 20:16:57 +0530

    Data stored in local storage stored in Table

--- studdata.js
+++ studdata.js
@@ -1,25 +1,39 @@
 /*Storing of data to local storage */
+ShowOnTable();
 function savetolocal(){
     if(localStorage.getItem("studarry") === null){
+    let data=[];
     let rowdata = ( $( document.getElementById("localStorageData") ).serializeArray());
-    let stringJSON = JSON.stringify(rowdata);
+    data.push(rowdata);
+    let stringJSON = JSON.stringify(data);
     localStorage.setItem("studarry",stringJSON);
     ShowOnTable();
     }
 /*Adding data to table from local storage*/
     else{
     let storedData = localStorage.getItem("studarry");
-    alert(storedData);
     let storedJSON = JSON.parse(storedData);
     let rowdatan = ( $( document.getElementById("localStorageData") ).serializeArray());
     storedJSON.push(rowdatan);
-    alert(storedJSON);
     let stringJSON1 = JSON.stringify(storedJSON);
     localStorage.setItem("studarry",stringJSON1);
     ShowOnTable();
     }
 }
 function ShowOnTable(){
-    let tbldata = document.getElementById("demo").innerHTML = localStorage.getItem("studarry");
-}
+    let storedData = localStorage.getItem("studarry");
+    let storedJSON = JSON.parse(storedData);
+    let i, name, dob, email, rno;
 
+    for(i=0; i<storedJSON.length ; i++)
+    {
+    name = storedJSON[i][0].value;
+    dob =  storedJSON[i][1].value;
+    email =  storedJSON[i][2].value;
+    rno =  storedJSON[i][3].value;
+    let data= `<tr><td>${name} </td><td>${dob} </td><td>${email} </td><td>${rno} </td></tr>`;
+    //let obj = document.getElementById("demo");
+    //obj.appendChild(data);
+    $(data).appendTo("#demo");
+}
+}
